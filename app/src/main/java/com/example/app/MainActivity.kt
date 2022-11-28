8fa59f5263113394cda91966f837b4d2b39d0ba6
Jameslawwd <jleo56291@gmail.com>
2022-11-28 11:17:05 +0300

    UserApp

---
+++ app/src/main/java/com/example/app/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
+
+//SignUp
+//Login
+
+// TextView -> Signup
+// 4 Edit Text -> username, email, phone ,                       password
+// Button -> SignUp
+// Already Registered? Login
+
+// TextView -> Login
+// 2 -edit Text -> email, password
+// Button -> Login
+
+// Not Registered yet? SignUp
+
+// Drawables -> Design
+// Angry tool
